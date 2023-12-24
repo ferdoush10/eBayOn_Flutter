07af1a2213bc03725fb1046e21822e434d95d71b
apple <ferdoushwahid74@gmail.com>
2023-12-25 02:57:38 +0600

    fetch data from local device

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'package:ebuy_on/models/catalog.dart';
+import 'package:ebuy_on/widget/iterm_widget.dart';
 import 'package:ebuy_on/widget/my_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,12 +14,41 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items =
+        List.from(productsData).map<Item>((e) => Item.fromMap(e)).toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Catalog app'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: CatalogModel.items[index],
+              );
+            }),
+      ),
       drawer: const MyDrawer(),
     );
   }
 }
+
+
+//TODO- 4.24 time next watch 
---
+++ lib/widget/iterm_widget.dart
@@ -0,0 +1,25 @@
+import 'package:ebuy_on/models/catalog.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price.toString()}",
+          style: const TextStyle(
+              fontSize: 18,
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
